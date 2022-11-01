a65286fafeac70b246dfe4774eed1344bd08188f
SonHaon <97341224+SonHaon@users.noreply.github.com>
2022-11-01 19:11:10 +0100

    rend invisible reboot

--- Cogs/bot/reboot.py
+++ Cogs/bot/reboot.py
@@ -20,11 +20,11 @@ class reboot(commands.Cog):
     @app_commands.command(name="reboot",description="redémarre le bot (et le raspberry)")
     @check.is_SonHaon()
     async def reboot(self,interaction:discord.Interaction):
+        await self.bot.change_presence(status=discord.Status.offline)
         await interaction.response.send_message("le bot va redémarrer",ephemeral=True)
         logger.info(f"'{interaction.user.display_name}' a redémarrer '{self.bot.user.display_name}' depuis le channel '{interaction.channel.name}'")
         webhook:discord.Webhook = await self.bot.channel.logs.webhooks()
         webhook = webhook[0]
         await webhook.send(f"{date_now()} j'ai **redémarrer** {self.bot.user.mention} depuis {interaction.channel.mention}",username=interaction.user.display_name,avatar_url=interaction.user.display_avatar.url)
-        await self.bot.change_presence(status=discord.Status.offline)
         os.system("sudo reboot")
         
