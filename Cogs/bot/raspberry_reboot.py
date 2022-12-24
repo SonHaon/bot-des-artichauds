d28e1785ad60fe71286db14ae38df6da174140fd
SonHaon <97341224+SonHaon@users.noreply.github.com>
2022-12-24 11:14:24 +0100

    fix raspberry_reboot

--- Cogs/bot/raspberry_reboot.py
+++ Cogs/bot/raspberry_reboot.py
@@ -40,7 +40,7 @@ class bouton_validation(discord.ui.View):
         row=0
     )
     async def not_reboot(self,interaction:discord.Interaction,button:discord.ui.Button):
-        await interaction.response.edit_message("Le raspberry n'a pas redémarré",view=None)
+        await interaction.response.edit_message(content="Le raspberry n'a pas redémarré",view=None)
 
 
 
