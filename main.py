2cddb6a90f77522768a9cd2618c8b537008db9a4
SonHaon <97341224+SonHaon@users.noreply.github.com>
2023-07-13 19:22:14 +0200

    fait nimp avec les trad

--- main.py
+++ main.py
@@ -52,6 +52,10 @@ class MyTranslator(Translator):
             if context.data.name == "ping":
                 return "testtesttest"
             return None
+        if locale is Locale.french and context.location is TranslationContextLocation.command_description:
+            if context.data.name == "verifie si le bot marche":
+                return translator.translate_text(string.message,target_lang="EN-US")
+            return None
         return None
     
 
