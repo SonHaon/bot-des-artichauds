078808efab04bb0450f67174c12ad6959205fa83
sonhaon <97341224+SonHaon@users.noreply.github.com>
2023-08-06 21:59:24 +0200

    ajout channel_history

--- Cogs/normal/channel_history.py
+++ Cogs/normal/channel_history.py
@@ -37,7 +37,7 @@ class channel_history(commands.Cog):
             async for message in interaction.channel.history():
                 file.writelines(f"{message}\n")
             file.close()
-            await interaction.edit_original_response("voila :)",attachments=[file])
+            await interaction.edit_original_response(content="voila :)",attachments=[file])
         except:
-            await interaction.edit_original_response("ca marche pas")
+            await interaction.edit_original_response(content="ca marche pas")
 
