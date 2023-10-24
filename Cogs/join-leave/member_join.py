94f01a49a27aa4768463326d2e938ac501c8ce51
sonhaon <97341224+SonHaon@users.noreply.github.com>
2023-10-24 14:21:59 +0200

    refonte message arrivée/depart

---
+++ Cogs/join-leave/member_join.py
@@ -0,0 +1,26 @@
+import asyncio
+from io import BytesIO 
+import discord 
+from discord.ext import  commands 
+from discord.app_commands import locale_str as _t
+from discord import  app_commands,ButtonStyle 
+from discord.ui import * 
+from PIL import Image, ImageDraw,ImageFont
+import os
+import json
+
+from fonction import embed
+
+
+
+class member_join(commands.Cog): 
+    def __init__(self,bot:commands.Bot) -> None: 
+        self.bot = bot 
+
+    @commands.Cog.listener(name="on_member_join")
+    async def on_member_join(self,member:discord.Member):
+        with open("info.json") as file:
+            channel_id=json.load(file)["channel_arrivee"]
+        channel = member.guild.get_channel(channel_id)
+        embeds = await embed(self.bot,member,"arrivee")
+        await channel.send(member.mention,embed=embeds)
--- Cogs/join-leave/member_leave.py
+++ Cogs/join-leave/member_leave.py
@@ -4,6 +4,9 @@ from discord.ext import  commands
 from discord import  app_commands,ButtonStyle 
 from discord.app_commands import locale_str as _t
 from discord.ui import * 
+import json
+
+from fonction import embed
 
 class member_leave(commands.Cog): 
     def __init__(self,bot:commands.Bot) -> None: 
@@ -11,4 +14,8 @@ class member_leave(commands.Cog):
 
     @commands.Cog.listener(name="on_member_remove")
     async def member_leave(self,member:discord.Member):
-        pass
+        with open("info.json") as file:
+            channel_id=json.load(file)["channel_depart"]
+        channel = member.guild.get_channel(channel_id)
+        embeds = await embed(self.bot,member,"depart")
+        await channel.send(member.mention,embed=embeds)
